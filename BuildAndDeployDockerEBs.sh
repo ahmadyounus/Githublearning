beafcd89764a21d40a9361dcf74d3294e0cd3c2d
ahmadyounus <ahmadyounus@gmail.com>
2020-06-27 10:08:12 +0100

    EB file update 1

--- BuildAndDeployDockerEBs.sh
+++ BuildAndDeployDockerEBs.sh
@@ -19,7 +19,8 @@ EBS_DEPLOY='
 }'
 
 echo $EBS_DEPLOY > Dockerrun.aws.json
-eb init --region eu-west-1 --platform docker tutorials1-ebs 
+export PATH=/root/.local/bin:$PATH
+eb init --region eu-west-1 --platform docker tutorials1-ebs
 
 # EB_Action : create or deploy
 eb create tutorials1-ebs  --region eu-west-1  
